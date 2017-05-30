d63891b9f8cf41bfa2f0a7ac9a62c39d018c3cea
Robin Linacre <robinlinacre@hotmail.com>
2017-05-30 13:16:27 +0100

    works

--- web/js/custom/namespace.js
+++ web/js/custom/namespace.js
@@ -1,4 +1,5 @@
 OCS_APP = {}
-OCS_APP.interface = null
-OCS_APP.checklist = null
-OCS_APP.datamanager	 = null
+OCS_APP.interface = null;
+OCS_APP.checklist = null;
+OCS_APP.datamanager	 = null;
+OCS_APP.table_to_markdown = null;
--- web/js/custom/table_to_md.js
+++ web/js/custom/table_to_md.js
@@ -1,8 +1,44 @@
 //
-TableToMarkdown = function() {
+var TableToMarkdown = function() {
+
+    var me = this;
+    this.rowdata = null
+
+
+
+    function row_to_md(row) {
+        var col1 = ""
+        var col2 = ""
+
+        col1 = row[0].value
+
+        if (row.length == 2) {
+            col2 = row[1].value
+        }
+
+        return(`|${col1}|${col2}|`)
+    }
 
     this.get_markdown = function() {
-        this.rowdata = OCS_APP.datamanager.get_d3_data()
+
+        //outputarray will hold the lines of the markdown table
+        
+        var column_names = OCS_APP.datamanager.column_names
+
+        me.rowdata = OCS_APP.datamanager.get_d3_data();
+        
+        var outputarray = []
+
+        outputarray.push(`|${column_names[0]}|${column_names[1]}|`)
+        outputarray.push("|---|---|")
+
+        _.each(me.rowdata, function(row) {
+            this_md = row_to_md(row)
+            outputarray.push(this_md)
+        })
+
+        return outputarray.join("\n")
+
     }
 
 
